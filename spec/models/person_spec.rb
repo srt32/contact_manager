b7e02a71c002324d75b25f57c84a3a9376b15b77
Simon Taranto <simon.taranto@gmail.com>
2013-11-26 16:27:16 -0700

    Fix false positive in person specs

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
 describe Person do
+  let(:person) do
+    Person.new(first_name: 'Alice', last_name: 'Smith')
+  end
 
   it 'is valid' do
-    person = Person.new(first_name: 'Alice', last_name: 'Smith')
-    expect(person).to be_valid
+    person.first_name = nil
+    expect(person).to_not be_valid
   end
 
   it 'is invalid without a first name' do
-    person = Person.new(first_name: nil)
-    expect(person).not_to be_valid
+    person.last_name = nil
+    expect(person).to_not be_valid
   end
 
   it 'is invalid without a last name' do
