bd70fcb248af2769daad77b2f3c397404f644fec
Simon Taranto <simon.taranto@gmail.com>
2013-11-27 09:18:10 -0700

    create Email model and associations with Person

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131127005232) do
+ActiveRecord::Schema.define(version: 20131127161530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "email_addresses", force: true do |t|
+    t.string   "address"
+    t.integer  "person_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: true do |t|
     t.string   "first_name"
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -23,4 +23,8 @@ describe Person do
     expect(person.phone_numbers).to eq([])
   end
 
+  it 'has an array of email addresses' do
+    expect(person.email_addresses).to eq([])
+  end
+
 end
