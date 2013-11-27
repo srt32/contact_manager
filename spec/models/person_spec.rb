d10d8d5d344c4bfe77e51099b6ed655d19db7be2
Simon Taranto <simon.taranto@gmail.com>
2013-11-27 12:08:55 -0700

    update person_spec association tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -20,11 +20,13 @@ describe Person do
   end
 
   it 'has an array of phone numbers' do
-    expect(person.phone_numbers).to eq([])
+    person.phone_numbers.build(number: '555-8888')
+    expect(person.phone_numbers.map(&:number)).to eq(['555-8888'])
   end
 
   it 'has an array of email addresses' do
-    expect(person.email_addresses).to eq([])
+    person.email_addresses.build(address: 'me@example.com')
+    expect(person.email_addresses.map(&:address)).to eq(['me@example.com'])
   end
 
 end
