0f32265a982284dcab94245384ab0f8a192a2d67
Simon Taranto <simon.taranto@gmail.com>
2013-11-26 18:05:34 -0700

    add uniqueness constraint to phone_number

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,3 @@
 class PhoneNumber < ActiveRecord::Base
+  validates :number, presence: true
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe PhoneNumber do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:phone_number) { PhoneNumber.new(number: "1112223333") }
+
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+
+  it 'is invalid without a number' do
+    phone_number.number = nil
+    expect(phone_number).to_not be_valid
+  end
+
 end
